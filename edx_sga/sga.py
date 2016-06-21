8b120f19e4b5896aa1fca1749488924a1cfa50b4
jianfaw <jianfaw@qq.com>
2016-06-21 22:30:45 +0800

    file encoding

--- edx_sga/sga.py
+++ edx_sga/sga.py
@@ -2,6 +2,8 @@
 This block defines a Staff Graded Assignment.  Students are shown a rubric
 and invited to upload a file which is then graded by staff.
 """
+# encoding=utf-8
+
 import datetime
 import hashlib
 import json
